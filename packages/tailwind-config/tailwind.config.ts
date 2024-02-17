ba1a05e102a0c4b0036d93aaee9113a5d149c3d2
sidarth-23 <sidarth157@gmail.com>
2024-02-17 12:09:16 +0530

    setup: tailwind default theme for all apps

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -10,7 +10,35 @@ const config = {
     "../../packages/ui/src/**/*.{ts,tsx}",
   ],
   prefix: "",
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT : "var(--primary)",
+          foreground: "var(--primary-foreground)",
+        },
+        secondary: {
+          DEFAULT : "var(--secondary)",
+          foreground: "var(--secondary-foreground)",
+        },
+        success: {
+          DEFAULT : "var(--success)",
+          foreground: "var(--success-foreground)",
+        },
+        danger: {
+          DEFAULT : "var(--danger)",
+          foreground: "var(--danger-foreground)",
+        },
+        warning: {
+          DEFAULT : "var(--warning)",
+          foreground: "var(--warning-foreground)",
+        },
+        focus: "var(--focus)",
+      }
+    }
+  },
   plugins: [],
 } satisfies Config;
 
