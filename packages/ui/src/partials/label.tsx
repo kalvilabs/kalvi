b3fe682be4a0898009b41aed21ba6d346339dc9c
sidarth-23 <sidarth157@gmail.com>
2024-02-21 13:40:38 +0530

    feat: add form utility components for easier form handling
    - Added a set of form utility components including Form, FormItem, FormLabel, FormControl, FormDescription, FormMessage, and FormField.
    - These components provide a convenient way to handle form fields, labels, descriptions, and error messages.
    - The react-hook-form library was added as a dependency to enable form validation and state management.
    feat: add new components for form handling and form validation
    
    - Added `FormItem` component to provide a context for form items
    - Added `FormMessage` component to display error messages for form fields
    - Added `ReactForm` object to export all form-related components
    - Added `FormLabel` component to handle form labels with error styling
    - Added `Form` component as a form provider
    - Added `FormField` component to handle form fields with context
    - Added `useFormField` hook to access form field context and state
    feat: update dependencies and reorganize form components
    
    - Updated dependencies to include "@hookform/resolvers" version 3.3.4
    - Reorganized form components into separate files for better organization and maintainability
    
    added previous commit to prevent merge conflict
    
    feat: made a separate package for react hook form and accomodated the ui package to fit it

--- packages/ui/src/partials/label.tsx
+++ packages/ui/src/partials/label.tsx
@@ -5,6 +5,7 @@ import * as LabelPrimitive from "@radix-ui/react-label";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "../../src/lib/tw-class-merge";
+import { FieldError } from "react-hook-form";
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -13,11 +14,11 @@ const labelVariants = cva(
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
+    VariantProps<typeof labelVariants> & {fieldError?: FieldError | undefined}
+>(({ className, fieldError = undefined, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={cn(labelVariants(), className)}
+    className={cn(labelVariants(), fieldError && 'text-red-500', className)}
     {...props}
   />
 ));
---
+++ packages/ui/src/react-form/form.tsx
@@ -0,0 +1,18 @@
+import { FormControl } from "./form-control";
+import { FormDescription } from "./form-description";
+import { Form, FormField, useFormField } from "./form-field";
+import { FormItem } from "./form-item";
+import { FormLabel } from "./form-label";
+import { FormMessage } from "./form-message";
+
+
+export const ReactForm = {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+}
