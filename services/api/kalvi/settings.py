19055d11dceec55c06f98cd04ee9a149821c3896
Shikhar Pandey <sankrityayan2k15me@gmail.com>
2024-02-23 11:21:05 +0530

    feat[bckend-auth]:Added SMTP configuration to send emails in case of forgotten passwords.

--- services/api/kalvi/settings.py
+++ services/api/kalvi/settings.py
@@ -159,3 +159,12 @@ SIMPLE_JWT = {
 }
 
 PASSWORD_RESET_TIMEOUT=900     #900 Sec = 15 Min
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+
