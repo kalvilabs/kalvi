ec9010f7fb3a2dab19f103b71122150ba444d276
sidarth-23 <sidarth157@gmail.com>
2024-02-28 14:21:03 +0530

    feat: update buttonVariants to include provider variant and add support for startIcon prop in Button component, chore: update labelVariants to include description

--- packages/ui/src/partials/button.tsx
+++ packages/ui/src/partials/button.tsx
@@ -3,13 +3,15 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "../../src/lib/tw-class-merge";
+import { LucideIcon } from "lucide-react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default: "grow bg-black text-white hover:bg-black/80",
+        provider: "border text-black hover:bg-black/5"
       },
       size: {
         default: "h-10 px-4 py-2"
@@ -31,6 +33,8 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
+  width?: 'full' | 'auto';
+  startIcon?: LucideIcon;
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
