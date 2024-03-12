e3e4e665ac3d9b4f6da537159bd215c7667311ac
sidarth-23 <sidarth157@gmail.com>
2024-03-12 20:38:21 +0530

    🚀 feat: add new Radix UI components and update styling partials for dashboard
    - Added `new Radix UI components` including react-avatar, 
    react-collapsible, react-dialog, and react-tooltip to the project 
    dependencies. 
    - The `Textbox` component's styling has been 
    updated to include different text sizes and font weights for better 
    customization options.
    - `Kalvi Logo` has been added to the ui package
    - `Button component` and `Textbox` has been given more control in variant including textSize, fontWeight and contentPosition to better be reused.
    - Variants have been added to `Card` to accomodate authentication page and dashboard
    - `Linktag` component has been given a size and contentPosition prop
    - `usehooks-ts` and `lucide-react` has been added to admin app and lock file updated
    
    🔧 refactor: convert Textbox component to use forwardRef for better performance

--- packages/ui/src/partials/button.tsx
+++ packages/ui/src/partials/button.tsx
@@ -6,20 +6,36 @@ import { cn } from "../lib/tw-class-merge";
 import { LucideIcon } from "lucide-react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center gap-3 whitespace-nowrap rounded-md ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default: "bg-black text-white hover:bg-black/80",
+        ghost: 'bg-transparent text-black hover:bg-black/5',
         provider: "border text-black hover:bg-black/5",
       },
       size: {
         default: "h-10 px-4 py-2",
+        icon: 'h-6 w-6'
+      },
+      textSize: {
+        default: "text-base",
+        sm: "text-sm",
+        lg: "text-lg",
+      },
+      fontWeight: {
+        default: 'font-medium',
+        normal: 'font-normal'
+      },
+      contentPosition: {
+        default: "justify-center",
+        start: "justify-start",
       },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      contentPosition: "default",
     },
   }
 );
@@ -39,6 +55,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       className,
       variant,
       size,
+      contentPosition,
+      textSize,
+      fontWeight,
       asChild = false,
       columnSpan,
       width,
@@ -52,7 +71,14 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Comp
         className={cn(
-          buttonVariants({ variant, size, className }),
+          buttonVariants({
+            variant,
+            size,
+            contentPosition,
+            textSize,
+            fontWeight,
+            className,
+          }),
           width === "full" && "w-full",
           columnSpan && `col-span-${columnSpan} grow`
         )}
---
+++ packages/ui/src/partials/sheet.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import * as React from "react";
+import * as SheetPrimitive from "@radix-ui/react-dialog";
+import { cva, type VariantProps } from "class-variance-authority";
+import { X } from "lucide-react";
+
+import { cn } from "../lib/tw-class-merge";
+
+const Sheet = SheetPrimitive.Root;
+
+const SheetTrigger = SheetPrimitive.Trigger;
+
+const SheetClose = SheetPrimitive.Close;
+
+const SheetPortal = SheetPrimitive.Portal;
+
+const SheetOverlay = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Overlay>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+  <SheetPrimitive.Overlay
+    className={cn(
+      "fixed inset-0 z-50 bg-white/40 backdrop-blur-sm ",
+      className
+    )}
+    {...props}
+    ref={ref}
+  />
+));
+SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
+
+const sheetVariants = cva(
+  "fixed z-50 gap-4 bg-white shadow-lg transition ease-in-out ",
+  {
+    variants: {
+      side: {
+        top: "inset-x-0 top-0 border-b",
+        bottom: "inset-x-0 bottom-0 border-t",
+        left: "inset-y-0 left-0 h-full border-r",
+        right: "inset-y-0 right-0 h-full  border-l",
+      },
+    },
+    defaultVariants: {
+      side: "right",
+    },
+  }
+);
+
+interface SheetContentProps
+  extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
+    VariantProps<typeof sheetVariants> {}
+
+const SheetContent = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Content>,
+  SheetContentProps
+>(({ side = "right", className, children, ...props }, ref) => (
+  <SheetPortal>
+    <SheetOverlay />
+    <SheetPrimitive.Content
+      ref={ref}
+      className={cn(sheetVariants({ side }), className)}
+      {...props}
+    >
+      {children}
+      <SheetPrimitive.Close className="absolute z-[100] right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
+        <X className="h-4 w-4" />
+        <span className="sr-only">Close</span>
+      </SheetPrimitive.Close>
+    </SheetPrimitive.Content>
+  </SheetPortal>
+));
+SheetContent.displayName = SheetPrimitive.Content.displayName;
+
+const SheetHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={className} {...props} />
+);
+SheetHeader.displayName = "SheetHeader";
+
+const SheetFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={className} {...props} />
+);
+SheetFooter.displayName = "SheetFooter";
+
+const SheetTitle = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <SheetPrimitive.Title
+    ref={ref}
+    className={cn("text-lg font-semibold", className)}
+    {...props}
+  />
+));
+SheetTitle.displayName = SheetPrimitive.Title.displayName;
+
+const SheetDescription = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Description>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <SheetPrimitive.Description ref={ref} className={className} {...props} />
+));
+SheetDescription.displayName = SheetPrimitive.Description.displayName;
+
+export {
+  Sheet,
+  SheetPortal,
+  SheetOverlay,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+};
