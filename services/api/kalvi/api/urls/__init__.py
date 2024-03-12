6409105d81911b241cda9d6d63de1e051ca1c0e5
Shikhar Pandey <77090827+shikharpa@users.noreply.github.com>
2024-03-12 15:19:49 +0530

    "Feature: GitHub OAuth Integration for Backend Authentication" (#78)
    * feat[bckend-auth]:Added Google Oauth configuration to login signin.
    
    * feat[bckend-auth]:Added Github Oauth configuration to login signin.

--- services/api/kalvi/api/urls/__init__.py
+++ services/api/kalvi/api/urls/__init__.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from kalvi.api.views import SignInEndPoint, SignUpEndPoint, UserProfileView, UserChangePasswordView, SendPasswordResetEmailView, UserPasswordResetView
-
+from db.Oauth.views import GoogleSocialAuthView, GithubSocialAuthView
 
 urlpatterns = [
     path(
@@ -32,5 +32,15 @@ urlpatterns = [
         "reset-password/<str:uid>/<str:token>/",
         UserPasswordResetView.as_view(),
         name="reset-password-through-mail",
+    ),
+    path(
+        "google/",
+        GoogleSocialAuthView.as_view(),
+        name="google-sign-in"
+    ),
+    path(
+        "github/",
+        GithubSocialAuthView.as_view(),
+        name="github-sign-in"
     )
 ]
