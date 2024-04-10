5a5f177420488c02f20573e0e43f3b563014024c
Shikhar Pandey <sankrityayan2k15me@gmail.com>
2024-04-10 14:55:51 +0530

    Feat[Bckend-db-fields]: Added a JSONField to each profile and organization setting for the theme and brand fields, and adjusted the views accordingly.

--- services/api/db/views.py
+++ services/api/db/views.py
@@ -22,7 +22,8 @@ class ProfileAPIView(APIView):
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except ValidationError as e:
+            return Response({"error" : e.detail}, status=status.HTTP_400_BAD_REQUEST)
         except Exception:
             return Response({'error': 'Please try again later!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
@@ -35,32 +36,21 @@ class OrganizationSettingsAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except OrganizationSettings.DoesNotExist:
             return Response({'error': "Sorry! we couldn't find organization"}, status=status.HTTP_404_NOT_FOUND)
-        
-    def post(self, request):
+
+    def put(self, request):
         try:
-            organization_settings = OrganizationSettings.objects.first()  #this returns none automatically if there is no object instance
-            if organization_settings:
-                return Response({'error': "organization already exists"}, status=status.HTTP_400_BAD_REQUEST)
-            serializer = OrganizationSettingsSerializer(data=request.data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
+            organization_settings, created = OrganizationSettings.objects.get_or_create(
+                defaults=request.data
+            )
+            if not created:
+                serializer = OrganizationSettingsSerializer(organization_settings, data=request.data)
+                if serializer.is_valid(raise_exception=True):
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                serializer = OrganizationSettingsSerializer(organization_settings)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except ValidationError as e:
             return Response({'error': e.detail}, status=status.HTTP_400_BAD_REQUEST)
         except Exception:
             return Response({'error': 'Please try again later!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def put(self, request):
-        try:
-            organization_settings = OrganizationSettings.objects.get()
-            serializer = OrganizationSettingsSerializer(organization_settings, data=request.data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except OrganizationSettings.DoesNotExist:
-            return Response({'error': "Sorry! we couldn't find organization"}, status=status.HTTP_404_NOT_FOUND)
-        except Exception:
-            return Response({'error': 'Please try again later!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
