6fe830619ae5ee160dcf3c63472fca1f82bde001
sidarth-23 <sidarth157@gmail.com>
2024-04-11 07:59:18 +0530

    ✨ Impr: Update global CSS variables for color scheme and styles
    - Changed primary, secondary, success, warning, and danger colors for a new color scheme. 
    - Also updated background, foreground, border colors, and added
    custom scrollbar styles.
    - Added tailwind forms for styling in admin and learner app
    
    Note: The changes are only till the tailwind config file is set in an extensible way

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindForms from "@tailwindcss/forms";
 
 const config = {
   darkMode: ["class"],
@@ -10,36 +11,44 @@ const config = {
     "../../packages/ui/src/**/*.{ts,tsx}",
   ],
   prefix: "",
-  theme: {
-    extend: {
+  extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: {
-          DEFAULT : "var(--primary)",
-          foreground: "var(--primary-foreground)",
-        },
-        secondary: {
-          DEFAULT : "var(--secondary)",
-          foreground: "var(--secondary-foreground)",
-        },
-        success: {
-          DEFAULT : "var(--success)",
-          foreground: "var(--success-foreground)",
-        },
-        danger: {
-          DEFAULT : "var(--danger)",
-          foreground: "var(--danger-foreground)",
-        },
-        warning: {
-          DEFAULT : "var(--warning)",
-          foreground: "var(--warning-foreground)",
-        },
-        focus: "var(--focus)",
+        "primary": 'rgba(var(--primary), <alpha-value>)',
+        "primary-foreground": 'rgba(var(--primary-foreground), <alpha-value>)',
+        "secondary": 'rgba(var(--secondary), <alpha-value>)',
+        "secondary-foreground": 'rgba(var(--secondary-foreground), <alpha-value>)',
+        "success": 'rgba(var(--success), <alpha-value>)',
+        "success-foreground": 'rgba(var(--success-foreground), <alpha-value>)',
+        "warning": 'rgba(var(--warning), <alpha-value>)',
+        "warning-foreground": 'rgba(var(--warning-foreground), <alpha-value>)',
+        "danger": 'rgba(var(--danger), <alpha-value>)',
+        "danger-foreground": 'rgba(var(--danger-foreground), <alpha-value>)',
+
+        "focus": 'rgba(var(--focus), <alpha-value>)',
+
+        "background": 'rgba(var(--background), <alpha-value>)',
+        "foreground": 'rgba(var(--foreground), <alpha-value>)',
+        "border-color": 'rgba(var(--border-color), <alpha-value>)',
+        "content-background": 'rgba(var(--content-background), <alpha-value>)',
+        "input-border-color": 'rgba(var(--input-border-color), <alpha-value>)',
+        "divider-color": 'rgba(var(--divider-color), <alpha-value>)',
+        'admin-bg-color': 'rgba(var(--admin-bg-color), <alpha-value>)',
+      },
+      spacing: {
+        xs: "8px",
+        s: "16px",
+        m: "24px",
+        l: "32px",
+        xl: "48px",
+        xxl: "80px"
+      },
+      borderRadius: {
+        small: "6px",
+        medium: "8px",
+        large: "32px"
       }
-    }
-  },
-  plugins: [],
+    },
+  plugins: [tailwindForms({strategy: "class"})],
 } satisfies Config;
 
 export default config;
