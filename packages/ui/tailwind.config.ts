e62001f628762688bb23aedd94f66ce88d5ba509
sidarth-23 <sidarth157@gmail.com>
2024-04-19 16:44:57 +0530

    ✨ Feat : Made a tailwind config customisable by api
    - Updated global CSS and theme variables to set primary and secondary colorsfor light and dark modes. 
    - Modified tailwind configuration in UI and app components, ensuring consistency of colors and styles.
    - Made the root layout to be able to load the variables from an api call
    ✨ Add custom theme variables for light and dark mode
    
    - Moved the css variable setting function into tailwind config package
    - Made a basic tailwind config and attached it to all relavant packages
    ✨ Update tailwindcss forms plugin
    
    Added the require statement for @tailwindcss/forms to the plugins array in the
    tailwind config file. This change ensures that only classes are generated by
    the forms plugin.

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,8 +1,17 @@
-export * from "@repo/tailwind-config/tw-config";
+import {type Config} from "tailwindcss";
+import { presetTheme } from "@repo/tailwind-config/tw-config";
 
-/**
- * Author: sidarth-23
- * Date: 2023-02-15
- * 
- * Any changes you want, make it in the tailwind config package so that it is consistent across all the apps
- */
+const config = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  presets: [presetTheme],
+} satisfies Config
+
+export default config
